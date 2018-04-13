22823de0c7f1cf89052cc95a0da0fedd82419440
kegilbert <kevin.gilbert@arm.com>
2018-04-13 12:24:29 -0500

    Move to public inheritance of SAI in children
    Allows access to general member methods like status.
    
    Moved internal functions to protected

--- drivers/SAI.h
+++ drivers/SAI.h
@@ -41,15 +41,18 @@ public:
         const sai_format_t *fmt = &sai_mode_i2s32, bool is_input = false,
         uint32_t master_clock = 0, bool internal_mclk = false);
 
-    /** Push a sample to the Fifo & try to read a new sample.
-     * it may return 0 if no sample was available.
-     */
-    virtual bool xfer(uint32_t *value);
-
     /** Retreive the object status. Allows the ctor to
      * funnel init errors/warnings back up to the application.
      */
     virtual sai_result_t status(void);
+
+    virtual ~SAI() {}
+
+protected:
+    /** Push a sample to the Fifo & try to read a new sample.
+     * it may return 0 if no sample was available.
+     */
+    virtual bool xfer(uint32_t *value);
 
     /** Acquire exclusive access to this SAI bus
      */
@@ -59,41 +62,29 @@ public:
      */
     virtual void unlock(void);
 
-
-public:
-    virtual ~SAI() {}
-
-protected:
+    ////////////////////////////////////////////
     sai_t           _sai;
     PlatformMutex   _mutex;
     bool            _is_input;
     sai_result_t    _status;
 };
 
-class SAITransmitter : private SAI {
+class SAITransmitter : public SAI {
     public:
         SAITransmitter(PinName mclk, PinName bclk, PinName wclk, PinName sd,
                        const sai_format_t *fmt = &sai_mode_i2s32)
            : SAI(mclk, bclk, wclk, sd, fmt, false) { }
-
-        sai_result_t status(void) {
-            return this->status();
-        }
 
         bool send(uint32_t sample) {
             return this->xfer(&sample);
         }
 };
 
-class SAIReceiver : private SAI {
+class SAIReceiver : public SAI {
     public:
         SAIReceiver(PinName mclk, PinName bclk, PinName wclk, PinName sd,
                     const sai_format_t *fmt = &sai_mode_i2s32)
             : SAI(mclk, bclk, wclk, sd, fmt, true) { }
-
-        sai_result_t status(void) {
-            return this->status();
-        }
 
         bool receive(uint32_t *sample) {
             return this->xfer(sample);
